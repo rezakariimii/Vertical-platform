3df6a2c9eb02886a442c6038cae4b1ad3e9db487
rezakariimii <rezakariimii1997@gmail.com>
2022-11-16 10:57:26 +0330

    create gravity

---
+++ index.js
@@ -0,0 +1,42 @@
+const canvas = document.querySelector(`canvas`);
+const c = canvas.getContext(`2d`);
+canvas.width = 1024;
+canvas.height = 576;
+const gravity = 0.5;
+
+class Player {
+  constructor(position) {
+      this.position = position;
+      this.velocity = {
+          x: 0,
+          y: 1
+      }
+      this.height = 100
+  }
+  draw() {
+    c.fillStyle = `red`;
+    c.fillRect(this.position.x, this.position.y, 100, this.height);
+  }
+  update() {
+    this.draw();
+      this.position.y += this.velocity.y;
+      if (this.position.y + this.height + this.velocity.y < canvas.height) {
+          this.velocity.y += gravity
+      } else {
+          this.velocity.y = 0;
+      }
+  }
+}
+const player = new Player({
+    x: 0,
+    y: 0
+});
+let y = 100;
+function animate() {
+  window.requestAnimationFrame(animate);
+  c.fillStyle = `#fff`;
+  c.fillRect(0, 0, canvas.width, canvas.height);
+
+  player.update();
+}
+animate();
