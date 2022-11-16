2e0fdc4103ddc4d0813f8e10b393d88bbea487dc
rezakariimii <rezakariimii1997@gmail.com>
2022-11-16 12:28:07 +0330

    background sprite

--- index.js
+++ index.js
@@ -3,15 +3,34 @@ const c = canvas.getContext(`2d`);
 canvas.width = 1024;
 canvas.height = 576;
 const gravity = 0.5;
+const scaledCanvas = {
+  width: canvas.width / 4,
+  height: canvas.height / 4,
+};
 
+class Sprite {
+  constructor({ position, imageSrc }) {
+    this.position = position;
+    this.image = new Image();
+    this.image.src = imageSrc;
+  }
+
+  draw() {
+    if (!this.image) return;
+    c.drawImage(this.image, this.position.x, this.position.y);
+  }
+  update() {
+    this.draw();
+  }
+}
 class Player {
   constructor(position) {
-      this.position = position;
-      this.velocity = {
-          x: 0,
-          y: 1
-      }
-      this.height = 100
+    this.position = position;
+    this.velocity = {
+      x: 0,
+      y: 1,
+    };
+    this.height = 100;
   }
   draw() {
     c.fillStyle = `red`;
@@ -19,24 +38,82 @@ class Player {
   }
   update() {
     this.draw();
-      this.position.y += this.velocity.y;
-      if (this.position.y + this.height + this.velocity.y < canvas.height) {
-          this.velocity.y += gravity
-      } else {
-          this.velocity.y = 0;
-      }
+    this.position.x += this.velocity.x;
+    this.position.y += this.velocity.y;
+    if (this.position.y + this.height + this.velocity.y < canvas.height) {
+      this.velocity.y += gravity;
+    } else {
+      this.velocity.y = 0;
+    }
   }
 }
 const player = new Player({
-    x: 0,
-    y: 0
+  x: 0,
+  y: 0,
 });
-let y = 100;
+
+const keys = {
+  d: {
+    pressed: false,
+  },
+  a: {
+    pressed: false,
+  },
+};
+
+const background = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  imageSrc: "./img/background.png",
+});
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = `#fff`;
   c.fillRect(0, 0, canvas.width, canvas.height);
 
+  c.save();
+  c.scale(4, 4);
+  c.translate(0, -background.image.height + scaledCanvas.height);
+  background.update();
+  c.restore();
+
   player.update();
+  player.velocity.x = 0;
+  if (keys.d.pressed) {
+    player.velocity.x = 5;
+  }
+  if (keys.a.pressed) {
+    player.velocity.x = -5;
+  }
 }
 animate();
+
+window.addEventListener(`keydown`, (e) => {
+  switch (e.key) {
+    case `d`:
+      keys.d.pressed = true;
+      break;
+    case `a`:
+      keys.a.pressed = true;
+      break;
+    case ` `:
+      player.velocity.y = -20;
+      break;
+    case `w`:
+      player.velocity.y = -20;
+      break;
+  }
+});
+
+window.addEventListener(`keyup`, (e) => {
+  switch (e.key) {
+    case `d`:
+      keys.d.pressed = false;
+      break;
+    case `a`:
+      keys.a.pressed = false;
+      break;
+  }
+});
